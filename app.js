8eb5d6d9e1455c1bba8f8ef0a28ef1cfcf43d19e
jaywomack <womackjw@hotmail.com>
2020-01-16 16:03:59 -0800

    Finished coding problems for chapter 5

--- app.js
+++ app.js
@@ -363,3 +363,116 @@
 //   }
 //   return true;
 // }
+
+/***
+ * 
+ //Chapter 5
+ * 
+ * 
+ * 
+ */
+
+// Flattening
+// let arrays = [[1, 2, 3], [4, 5], [6]];
+
+// // function concatArrays(array) {
+// //   let newArray = [];
+// //   array.reduce(arr => {
+// //     console.log(arr);
+// //     newArray.concat(arr);
+// //   });
+// //   return newArray;
+// // }
+
+// // Books version
+// function concatArrays(array) {
+//   return array.reduce((flat, current) => flat.concat(current), []);
+// }
+// console.log(concatArrays(arrays));
+/***
+ *
+ *
+ *
+ * YOUR OWN LOOP
+ */
+// function loop(start, test, update, body) {
+//   for (let value = start; test(value); value = update(value)) {
+//     body(value);
+//   }
+// }
+
+// loop(
+//   3,
+//   n => n > 0,
+//   n => n - 1,
+//   console.log
+// );
+
+/***
+ *
+ *
+ * EVERYTHING
+ */
+// solution from the book
+// function every(array, predicate) {
+//   for (let element of array) {
+//     if (!predicate(element)) return false;
+//   }
+//   return true;
+// }
+
+// function every2(array, predicate) {
+//   return !array.some(element => !predicate(element));
+// }
+// console.log(every2([1, 3, 5], n => n < 10));
+// // → true
+// console.log(every2([2, 4, 16], n => n < 10));
+// // → false
+// console.log(every2([], n => n < 10));
+// // → true
+
+/***
+ *
+ *
+ * Dominant writing direction
+//  */
+// function countBy(items, groupName) {
+//   let counts = [];
+//   for (let item of items) {
+//     let name = groupName(item);
+//     let known = counts.findIndex(c => c.name == name);
+//     if (known == -1) {
+//       counts.push({ name, count: 1 });
+//     } else {
+//       counts[known].count++;
+//     }
+//   }
+// }
+
+// function characterScript(code) {
+//   for (let script of SCRIPTS) {
+//     if (
+//       script.ranges.some(([fom, to]) => {
+//         return code >= from && code < to;
+//       })
+//     ) {
+//       return script;
+//     }
+//   }
+//   return null;
+// }
+
+// function dominantDirection(text) {
+//   let counted = countBy((text, char) => {
+//     let script = characterScript(char.codePointAt(0));
+//     return script ? script.direction : "none";
+//   }).filter(({ name }) => name != "none");
+
+//   if (counted.lenght == 0) return " ltr";
+//   return counted.reduce((a, b) => (a.count > b.count ? a : b)).name;
+// }
+
+// console.log(dominantDirection("Hello!"));
+// // → ltr
+// console.log(dominantDirection("Hey, مساء الخير"));
+// // → rtl
